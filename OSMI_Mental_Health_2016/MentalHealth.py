2a524d7888ee2c5e8571531f6265c8d06e094053
Julio Cardenas <cardenaj@email.arizona.edu>
2017-03-04 10:31:02 -0700

    Initial Who answered

---
+++ OSMI_Mental_Health_2016/MentalHealth.py
@@ -0,0 +1,6 @@
+"""Analysis of OSMI: Mental Health Tech 2016."""
+__author__      = "Julio Cárdenas-Rodríguez"
+__License__     = "MIT"
+
+import pandas as pd
+df = pd.read_csv('https://query.data.world/s/bs6aqtm2l54gty0ng1vsgw37k')
